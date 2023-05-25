a52e5be90e899fa5543e5bfabf86b6f79abe10fb
Alex Archambault <alexandre.archambault@gmail.com>
2023-05-25 16:30:33 +0200

    Switch to Scala CLI
    Getting weird publishing issues with Mill

---
+++ project.scala
@@ -0,0 +1,5 @@
+//> using scala "3.2.2"
+//> using jvm "8"
+//> using dep "com.lihaoyi::os-lib:0.9.1"
+//> using dep "io.get-coursier::dependency:0.2.2"
+//> using dep "org.virtuslab:using_directives:1.1.0"
